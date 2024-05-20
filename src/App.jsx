3c8fb9ea398a6812bb4794591edd7a7692432653
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-05-20 17:31:48 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
+import css from './App.css'
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedbackPercentage = totalFeedback ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  return (
+    <div className={css.App}>
+      <h1 className={css.title}>Sip Happens Café</h1>
+      <p className={css.text}>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedbackPercentage={positiveFeedbackPercentage} />
+      ) : (
+        <Notification message="No feedback given yet." />
+      )}
+    </div>
+  );
+};
+
+export default App;
